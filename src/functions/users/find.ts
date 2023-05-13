bc0acf24ea34a3db1eee4fffb5796e8ec3392b0c
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-13 01:04:30 -0500

    :fire: remove files unnecessary

--- src/functions/users/find.ts
+++
@@ -1,6 +0,0 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
-import UserService from "../../services/user.service";
-export const handler: APIGatewayProxyHandler = async (event) => {
-  const { id: userId }: any = event.pathParameters;
-  return await UserService.findById(userId);
-};
