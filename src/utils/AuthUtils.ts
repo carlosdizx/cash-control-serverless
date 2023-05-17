a5faf943a57239d92ad9d75b6407c42be9593621
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-17 15:08:31 -0500

    :sparkles: optimize code and instal types for jsonwebtoken

--- src/utils/AuthUtils.ts
+++ src/utils/AuthUtils.ts
@@ -1,21 +1,17 @@
-import jwt from 'jsonwebtoken';
+import jwt, {JwtPayload} from 'jsonwebtoken';
 import dotenv = require("dotenv");
 import User from "../entities/User.entity";
 
 dotenv.config();
 
 const JWT_SECRET = process.env.JWT_SECRET;
+const JWT_EXPIRES = process.env.JWT_EXPIRES;
 
-const generateToken = async (user: User) => {
-    return jwt.sign(user, JWT_SECRET, {expiresIn: '1d'});
-}
+const generateToken = async (user: User) =>
+    jwt.sign(user, JWT_SECRET, {expiresIn: JWT_EXPIRES});
 
-const verifyToken = (token) => {
-    try {
-        return jwt.verify(token, JWT_SECRET);
-    } catch (err) {
-        return null;
-    }
-}
+const verifyToken = (token) =>
+    jwt.verify(token, JWT_SECRET);
+
 
 export { generateToken, verifyToken };
