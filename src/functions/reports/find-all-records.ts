fe4de4cddf48425fc971b9ac70bd0e87544bc831
Carlos Díaz <carlodiaz@umariana.edu.co>
2023-05-17 02:28:11 -0500

    :fire: remvoe code

--- src/functions/reports/find-all-records.ts
+++
@@ -1,9 +0,0 @@
-import { APIGatewayProxyHandler } from "aws-lambda";
-import responseObject from "../../utils/Response";
-import ReportRecordsService from "../../services/report.records.service";
-export const handler: APIGatewayProxyHandler = async (event, context) => {
-    console.log(`HANDLER: Starting ${context.functionName}...`);
-    if (event.queryStringParameters)
-        return ReportRecordsService.findAllRecords(event.queryStringParameters);
-    return responseObject(400, {message: "Query parameters are required"});
-};
